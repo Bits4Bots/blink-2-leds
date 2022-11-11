2b8a42e5d1131d266250bdb8340b25c35743588a
Bits4Bots LLC <bits4bots@gmail.com>
2022-11-11 07:52:26 -0500

    Blink four pins. P0, P1, P2, P8

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
     basic.pause(200)
     pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
     basic.pause(200)
 })
